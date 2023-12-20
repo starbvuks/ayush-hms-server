4dd96b326cdf7036077d8b6da80b8ec4760258e7
starbvuks <starbvuks@gmail.com>
2023-12-20 12:24:46 +0530

    latest

--- routes/admin/adminDispensaryEntries.js
+++ routes/admin/adminDispensaryEntries.js
@@ -3,25 +3,6 @@ const router = express.Router();
 const db = require("../../db/index");
 
 router.get(
-  "/dispensaries/:dispensaryId/patient-entries",
-  async (req, res, next) => {
-    const dispensaryId = req.params.dispensaryId;
-    try {
-      const entries = await db.query(
-        `SELECT * FROM patiententry WHERE dispensary_id = $1 ORDER BY entry_date DESC`,
-        [dispensaryId]
-      );
-      res.json(entries.rows);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        message: "An error occurred while fetching patient entries",
-      });
-    }
-  }
-);
-
-router.get(
   "/dispensaries/:dispensaryId/patient-entries/:timeframe",
   async (req, res, next) => {
     const dispensaryId = req.params.dispensaryId;
@@ -57,40 +38,48 @@ router.get(
   }
 );
 
-router.get("/admin/dispensaries-entry/search", async (req, res) => {
-  try {
-    const { searchTerm, timeframe } = req.query;
-    let query = "SELECT * FROM patiententry";
-    let params = [];
+router.get(
+  "/admin/dispensaries-entry/:dispensaryId/search",
+  async (req, res) => {
+    try {
+      const { searchTerm, timeframe } = req.query;
+      const dispensaryId = req.params.dispensaryId;
+      let query = "SELECT * FROM patiententry";
+      let params = [dispensaryId];
 
-    if (timeframe) {
-      if (timeframe === "today") {
-        query += " WHERE DATE(entry_date) = CURRENT_DATE";
-      } else if (timeframe === "lastweek") {
-        query += " WHERE entry_date >= NOW() - INTERVAL '7 days'";
-      } else if (timeframe === "lastmonth") {
-        query += " WHERE entry_date >= NOW() - INTERVAL '1 month'";
-      } else if (timeframe === "lastyear") {
-        query += " WHERE entry_date >= NOW() - INTERVAL '1 year'";
-      } else if (timeframe === "alltime") {
-        // No additional condition needed
+      if (timeframe) {
+        if (timeframe === "today") {
+          query +=
+            " WHERE dispensary_id = $1 AND DATE(entry_date) = CURRENT_DATE";
+        } else if (timeframe === "lastweek") {
+          query +=
+            " WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '7 days'";
+        } else if (timeframe === "lastmonth") {
+          query +=
+            " WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '1 month'";
+        } else if (timeframe === "lastyear") {
+          query +=
+            " WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '1 year'";
+        } else if (timeframe === "alltime") {
+          // No additional condition needed
+        }
       }
-    }
 
-    if (searchTerm) {
-      query +=
-        " AND (first_name ILIKE $1 OR last_name ILIKE $1 OR diagnosis ILIKE $1 OR treatment ILIKE $1 OR other_info ILIKE $1)";
-      params.push(`%${searchTerm}%`);
-    }
+      if (searchTerm) {
+        query +=
+          " AND (first_name ILIKE $2 OR last_name ILIKE $2 OR diagnosis ILIKE $2 OR treatment ILIKE $2 OR other_info ILIKE $2)";
+        params.push(`%${searchTerm}%`);
+      }
 
-    const entries = await db.query(query, params);
-    res.json(entries.rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      message: "An error occurred while fetching patient entries",
-    });
+      const entries = await db.query(query, params);
+      res.json(entries.rows);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "An error occurred while fetching patient entries",
+      });
+    }
   }
-});
+);
 
 module.exports = router;
