191893aac6df661338d24dacc595bffbdc22bd14
starbvuks <starbvuks@gmail.com>
2024-01-04 13:36:20 +0530

    search all

--- routes/admin/adminDispensaryEntries.js
+++ routes/admin/adminDispensaryEntries.js
@@ -10,18 +10,37 @@ router.get(
     const page = req.query.page || 1;
     const pageSize = req.query.pageSize || 7;
     let query = "";
-    let values = [dispensaryId];
+    let values = [];
+
+    if (dispensaryId !== "*") {
+      values.push(dispensaryId);
+    }
 
     if (timeframe === "today") {
-      query = `SELECT * FROM patiententry WHERE dispensary_id = $1 AND DATE(entry_date) = CURRENT_DATE ORDER BY entry_date DESC`;
+      query =
+        dispensaryId === "*"
+          ? `SELECT * FROM patiententry WHERE DATE(entry_date) = CURRENT_DATE ORDER BY entry_date DESC`
+          : `SELECT * FROM patiententry WHERE dispensary_id = $1 AND DATE(entry_date) = CURRENT_DATE ORDER BY entry_date DESC`;
     } else if (timeframe === "lastweek") {
-      query = `SELECT * FROM patiententry WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '7 days' ORDER BY entry_date DESC`;
+      query =
+        dispensaryId === "*"
+          ? `SELECT * FROM patiententry WHERE entry_date >= NOW() - INTERVAL '7 days' ORDER BY entry_date DESC`
+          : `SELECT * FROM patiententry WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '7 days' ORDER BY entry_date DESC`;
     } else if (timeframe === "lastmonth") {
-      query = `SELECT * FROM patiententry WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '1 month' ORDER BY entry_date DESC`;
+      query =
+        dispensaryId === "*"
+          ? `SELECT * FROM patiententry WHERE entry_date >= NOW() - INTERVAL '30 days' ORDER BY entry_date DESC`
+          : `SELECT * FROM patiententry WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '30 days' ORDER BY entry_date DESC`;
     } else if (timeframe === "lastyear") {
-      query = `SELECT * FROM patiententry WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '1 year' ORDER BY entry_date DESC`;
+      query =
+        dispensaryId === "*"
+          ? `SELECT * FROM patiententry WHERE entry_date >= NOW() - INTERVAL '364 days' ORDER BY entry_date DESC`
+          : `SELECT * FROM patiententry WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '364 days' ORDER BY entry_date DESC`;
     } else if (timeframe === "alltime") {
-      query = `SELECT * FROM patiententry WHERE dispensary_id = $1 ORDER BY entry_date DESC`;
+      query =
+        dispensaryId === "*"
+          ? `SELECT * FROM patiententry ORDER BY entry_date DESC`
+          : `SELECT * FROM patiententry WHERE dispensary_id = $1 ORDER BY entry_date DESC`;
     }
 
     try {
@@ -56,10 +75,10 @@ router.get(
             " WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '7 days'";
         } else if (timeframe === "lastmonth") {
           query +=
-            " WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '1 month'";
+            " WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '30 days'";
         } else if (timeframe === "lastyear") {
           query +=
-            " WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '1 year'";
+            " WHERE dispensary_id = $1 AND entry_date >= NOW() - INTERVAL '364 days'";
         } else if (timeframe === "alltime") {
           // No additional condition needed
         }
@@ -85,5 +104,41 @@ router.get(
     }
   }
 );
+
+router.get("/admin/all-dispensaries-entry/search", async (req, res) => {
+  try {
+    const { searchTerm, timeframe } = req.query;
+    let query = "SELECT * FROM patiententry";
+    let params = [];
+
+    if (timeframe) {
+      if (timeframe === "today") {
+        query += " WHERE DATE(entry_date) = CURRENT_DATE";
+      } else if (timeframe === "lastweek") {
+        query += " WHERE entry_date >= NOW() - INTERVAL '7 days'";
+      } else if (timeframe === "lastmonth") {
+        query += " WHERE entry_date >= NOW() - INTERVAL '30 days'";
+      } else if (timeframe === "lastyear") {
+        query += " WHERE entry_date >= NOW() - INTERVAL '364 days'";
+      }
+    }
+
+    if (searchTerm) {
+      query += ` AND EXISTS (
+       SELECT 1 FROM unnest(string_to_array($${params.length + 1}, ' ')) AS term
+       WHERE tsv @@ phraseto_tsquery('english', term)
+       )`;
+      params.push(searchTerm);
+    }
+
+    const entries = await db.query(query, params);
+    res.json(entries.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "An error occurred while fetching patient entries",
+    });
+  }
+});
 
 module.exports = router;
